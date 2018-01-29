88ab0f7faac9b166f34d7b3b9e40fd4d4b57062b
Raja <raja.muthukalai@yahoo.com>
2018-01-29 22:53:23 +0530

    added hystrix dependency

--- src/main/java/com/sannu/billerservice/BillerServiceApplication.java
+++ src/main/java/com/sannu/billerservice/BillerServiceApplication.java
@@ -3,12 +3,14 @@ package com.sannu.billerservice;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 public class BillerServiceApplication {
 	
